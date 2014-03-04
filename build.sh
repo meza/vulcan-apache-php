b7cab44e5c7b0db9cb69821db0356e26de86d2ba
meza <meza@meza.hu>
2014-03-04 01:42:56 +0100

    apache-php

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 vulcan update
-sh ./get_pagespeed
-vulcan build -v -p /app -o "$PWD"/package.tgz -s . -c "sh vulcan_build" > ../output.log 2> ../error.log
+#sh ./get_pagespeed
+time vulcan build -v -p /app -o "$PWD"/package.tgz -s . -c "sh vulcan_build"
+# > ../output.log 2> ../error.log
