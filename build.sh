a6a0e23b3524ddb142f4604326ba6e72180a75e6
meza <meza@meza.hu>
2014-03-06 12:37:18 +0100

    more verbosity

--- build.sh
+++ build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
+echo "Cleaning directory"
+rm -rf *.tar.gz
+rm -rf *.tgz
+
+echo "Updating vulcan"
 vulcan update
 #sh ./get_pagespeed
+
+echo "Building the package"
 time vulcan build -v -p /app -o "$PWD"/package.tgz -s . -c "sh vulcan_build"
 # > ../output.log 2> ../error.log
